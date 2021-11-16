6bb3175b8d03af15ecd1226793015380533c8f94
Florian Gaffke <florek.gaffke@gmail.com>
2021-11-16 22:47:22 +0100

    Add server task: main, detection

---
+++ src/imshowtest.py
@@ -0,0 +1,4 @@
+import cv2
+def show(img):
+    cv2.imshow("hello", img)
+    cv2.waitKey(0)
--- src/main.py
+++ src/main.py
@@ -1,16 +1,36 @@
-from flask import Flask
-from flask_restful import Api, Resource
+import base64
+import numpy as np
+from flask import Flask, request, render_template
+import cv2
+import  imshowtest
 
 app = Flask(__name__)
-api = Api(app)
 
-#defining requests
-class HelloWorld(Resource):
-    def get(self):
-        return {"data": "Hello World"}
 
-api.add_resource(HelloWorld, "/helloworld")
+# main pagge
+@app.route('/')
+def main():
+    return render_template('main.html')
 
-#start our application
+
+# detection
+@app.route('/detection', methods=['POST', 'GET'])
+def detection():
+    if request.method == 'POST':
+        uploaded_file = request.files['formFile']
+        if not uploaded_file:
+            return "No file uploaded", 400
+        im_b64 = base64.b64encode(uploaded_file.read())
+        im_bytes = base64.b64decode(im_b64)
+        im_arr = np.frombuffer(im_bytes, dtype=np.uint8)  # im_arr is one-dim Numpy array
+        img = cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
+        imshowtest.show(img)
+
+        return render_template('detection.html')
+    else:
+        return render_template('detection.html')
+
+
+# start our application
 if __name__ == "__main__":
     app.run(debug=True)
